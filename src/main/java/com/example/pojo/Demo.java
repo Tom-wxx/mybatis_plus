9db144939fbaaef5460bf5d85dc5b2e1270a3ba9
wxx <1301275402@qq.com>
2021-06-18 17:14:10 +0800

    测试添加

---
+++ src/main/java/com/example/pojo/Demo.java
@@ -0,0 +1,12 @@
+package com.example.pojo;
+
+import lombok.Data;
+
+@Data
+public class Demo {
+    private Long id;
+    private String  name;
+    private Integer age;
+    private String  password;
+    private String email;
+}
