42c5a0fdadadd0cea91d73338cd3ed75c90ca4c6
wxx <1301275402@qq.com>
2021-06-22 14:04:08 +0800

    测试添加

--- src/main/java/com/example/pojo/AppClass.java
+++ src/main/java/com/example/pojo/AppClass.java
@@ -5,4 +5,11 @@ import lombok.Data;
 @Data
 public class AppClass {
     private String name;
+    //    身份证
+    private String code;
+    //    手机
+    private String phone;
+    private String uuid;
+    //    验证码
+    private String captcha;
 }
