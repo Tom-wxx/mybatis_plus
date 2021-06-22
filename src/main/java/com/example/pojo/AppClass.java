7e4d78bef628598f1874d262620e91e9b93901f8
wxx <1301275402@qq.com>
2021-06-22 13:57:05 +0800

    cshi

--- src/main/java/com/example/pojo/AppClass.java
+++ src/main/java/com/example/pojo/AppClass.java
@@ -5,4 +5,12 @@ import lombok.Data;
 @Data
 public class AppClass {
     private String name;
+    private String name1;
+    private String name2;
+    private String name3;
+    private String name4;
+
+
+
+
 }
